d61539d070fa1813c0d145c242ff8765a8de1d27
Camilo Pinzon <pinzonac@gmail.com>
2023-09-13 19:08:33 -0500

    create server, create api and templating

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
+const fs = require("fs");
 const http = require("http");
+const urlLib = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 
 const server = http.createServer((request, response) => {
-  console.log(request);
-  response.end("Hello from the server!");
+  const pathName = request.url;
+
+  if (pathName === "/overview" || pathName === "/")
+    response.end(`this is OVERVIEW`);
+  else if (pathName === "/product") response.end("this is: PRODUCT");
+  else if (pathName === "/api") {
+    if (data) {
+      response.writeHead(200, { "content-type": "application/json" });
+      response.end(data);
+    }
+  } else {
+    response.writeHead(404, { "content-type": "text/html" });
+    response.end("<h1>404 ERROR</h1></br><h2>Page not found!</h2>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
